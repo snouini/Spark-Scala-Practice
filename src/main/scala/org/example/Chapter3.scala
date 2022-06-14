0c039a5ed35f53d44dde074eb45b56e55e963ba1
snouini <snouini19@gmail.com>
2022-06-14 20:50:50 +0200

    Second commit

--- src/main/scala/org/example/Chapter3.scala
+++ src/main/scala/org/example/Chapter3.scala
@@ -1,9 +1,12 @@
 package org.example
 import org.example.schemas._
-import org.apache.spark.sql.{SparkSession, types, _}
+import org.apache.spark.sql.{SparkSession, types, functions => F}
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql
+import org.apache.spark.sql.catalyst.expressions.Like
 import org.apache.spark.sql.expressions._
 import org.apache.spark.sql.types.IntegerType
+
 
 
 
@@ -17,7 +20,7 @@ object Chapter3 {
     import spark.implicits._
 
     val blogs = spark.read.schema(schema2)
-      .json("src/main/resources/blogs.json")
+      .json("src/main/resources/chapter3/blogs.json")
 
     /* using expression to compute a value*/
     blogs.select(expr("Hits*2")).show(2)
@@ -37,21 +40,95 @@ object Chapter3 {
     blogRowdf.show()
 
 
-    val fs_df = spark.read.option("header","true").schema(schema3)
-      .csv("src/main/resources/financial-survey.csv")
+    val fire_df = spark.read.option("header","true").schema(schema3)
+      .csv("src/main/resources/chapter3/sf-fire-calls.csv")
 
-    fs_df.show(truncate = false)
+    /*val fire_parquet_file = fire_df.write.format("parquet").save("src/main/saved_parquet_files/fire_df")*/
 
-    val new_fs_df = fs_df.withColumn("new_value" , $"value".cast(IntegerType)).drop(col("value"))
+    /*val fire_parquet_table = "fire_parquet_table"
+    fire_df.write.format("parquet").saveAsTable(fire_parquet_table)*/
 
-    new_fs_df.select("new_value").where($"new_value" > 5).show()
+    val fewfiredf = fire_df.select("IncidentNumber", "AvailableDtTm" , "CallType")
+      .where(col("CallType") =!= "Medical Incident")
 
-    val fs_df_ = new_fs_df.withColumn("Date" , lit("05-06-2022"))
 
-    val fs_df_ts = fs_df_.withColumn("new_date" , to_timestamp(col("Date") , "MM/dd/yy"))
-      .drop("Date")
+    fewfiredf.show(5,false)
 
-    fs_df_ts.show()
+    fire_df.select("CallType").where(col("CallType").isNotNull)
+      .agg(countDistinct("CallType") as "DistinctCallTypes").show()
+
+
+    /*Renaming, adding and dropping columns*/
+
+    val newfiredf = fire_df.withColumnRenamed("Delay", "ResponseDelayedinMins")
+
+    newfiredf
+      .select("ResponseDelayedinMins")
+      .where($"ResponseDelayedinMins" > 5)
+      .show(5,false)
+
+    /*Changing string date columns to timestamp*/
+
+    val fireTsDF = newfiredf
+      .withColumn("IncidentDate", to_timestamp(col("CallDate"), "MM/dd/yyyy"))
+      .drop("CallDate")
+      .withColumn("OnWatchDate", to_timestamp(col("WatchDate"), "MM/dd/yyyy"))
+      .drop("WatchDate")
+      .withColumn("AvailableDtTS", to_timestamp(col("AvailableDtTm"),
+        "MM/dd/yyyy hh:mm:ss a"))
+      .drop("AvailableDtTm")
+
+    fireTsDF.select("IncidentDate", "OnWatchDate", "AvailableDtTS").show(5,false)
+
+    fireTsDF.select(year($"IncidentDate")).distinct().orderBy(year($"IncidentDate")).show()
+
+    fireTsDF
+      .select("CallType").where($"CallType".isNotNull)
+      .groupBy("CallType").count().orderBy(desc("count"))
+      .show()
+
+    fireTsDF.select(F.sum("NumAlarms"), round(F.avg("ResponseDelayedinMins"),2),
+      round(F.min("ResponseDelayedinMins") ,2) ,
+      round(F.max("ResponseDelayedinMins") ,2) ).show()
+
+    /* End-to-end example*/
+
+    /* Different types of fire calls in 2018*/
+
+    fireTsDF.select("CallType", "IncidentDate")
+      .where(year($"IncidentDate") === 2018)
+      .distinct().show()
+
+    /*What months within the year 2018 saw the highest number of fire calls?*/
+
+    fireTsDF.select("IncidentDate").where(year($"IncidentDate") === 2018)
+      .groupBy(month($"IncidentDate")).count().orderBy(desc("count")).show()
+
+    /* Which neighborhood in San Francisco generated the most fire calls in 2018? */
+
+    fireTsDF.select("Neighborhood")
+      .where(year($"IncidentDate") === 2018)
+      .groupBy("Neighborhood").count().orderBy(desc("count")).show()
+
+    /*Which week in the year in 2018 had the most fire calls?*/
+
+    fireTsDF.select("IncidentDate","CallType")
+      .where(year($"IncidentDate") === 2018)
+      .groupBy(weekofyear($"IncidentDate")).agg(count("CallType"))
+      .orderBy(desc("count(CallType)")).show()
+
+    /*Is there a correlation between neighborhood, zip code, and number of fire calls?*/
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/main/scala/org/example/schemas.scala
+++ src/main/scala/org/example/schemas.scala
@@ -15,10 +15,20 @@ object schemas {
     StructField("Url", StringType, false), StructField("Published", StringType, false),
     StructField("Hits", IntegerType, false),StructField("Campaigns", ArrayType(StringType), false))))
 
-  val schema3 = StructType(Array(StructField("year", StringType, true),
-    StructField("industry_code_ANZSIC", StringType , true), StructField("industry_name_ANZSIC", StringType , true),
-    StructField("rme_size_grp", StringType , true) , StructField("variable", StringType , true),
-    StructField("value", StringType , true), StructField("Unit", StringType , true)))
-
+  val schema3 = StructType(Array(StructField("CallNumber", IntegerType, true),
+    StructField("UnitID", StringType , true), StructField("IncidentNumber", IntegerType , true),
+    StructField("CallType", StringType , true) , StructField("CallDate", StringType , true),
+    StructField("WatchDate", StringType , true), StructField("CallFinalDisposition", StringType , true),
+    StructField("AvailableDtTm", StringType , true),StructField("Address", StringType , true),
+    StructField("City", StringType , true),StructField("Zipcode", IntegerType , true),
+    StructField("Battalion", StringType, true), StructField("StationArea", StringType, true),
+    StructField("Box", StringType, true), StructField("OriginalPriority", StringType, true),
+    StructField("Priority", StringType, true), StructField("FinalPriority", IntegerType, true),
+    StructField("ALSUnit", BooleanType, true), StructField("CallTypeGroup", StringType, true),
+    StructField("NumAlarms", IntegerType, true), StructField("UnitType", StringType, true),
+    StructField("UnitSequenceInCallDispatch", IntegerType, true),
+    StructField("FirePreventionDistrict", StringType, true), StructField("SupervisorDistrict", StringType, true),
+    StructField("Neighborhood", StringType, true), StructField("Location", StringType, true),
+    StructField("RowID", StringType, true), StructField("Delay", FloatType, true)))
 }
 
